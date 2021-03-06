a6ce2aec7b56de81330c0074ee61f4dc0cb2aa30
Jose Guillermo Gutierrez <jose.gutierrez@rappi.com>
2021-03-05 19:47:16 -0600

    feat: remove storyboard and add popularMoviesTableView

--- PlatziTest/ViewController.swift
+++ PlatziTest/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let popularMoviesTableView = UITableView()
+    
+    var characters = ["Movie1", "Movie2", "Movie3", "Movie4"]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
+    
+    override func loadView() {
+        super.loadView()
+        setupTableView()
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "popularMovieCell", for: indexPath)
+        cell.textLabel?.text = characters[indexPath.row]
+        return cell
+    }
+}
 
+private extension ViewController {
+    func setupTableViewConstraints() {
+        view.addSubview(popularMoviesTableView)
+        popularMoviesTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            popularMoviesTableView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            popularMoviesTableView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            popularMoviesTableView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            popularMoviesTableView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
+    func setupTableView() {
+        popularMoviesTableView.dataSource = self
+        popularMoviesTableView.register(UITableViewCell.self, forCellReuseIdentifier: "popularMovieCell")
+        setupTableViewConstraints()
+    }
 }
 
